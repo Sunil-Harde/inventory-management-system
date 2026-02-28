3ed468a88b4254657b0e7d06d50c26dce1ae7020
Sunil-Harde <158262002+Sunil-Harde@users.noreply.github.com>
2026-02-28 17:39:36 +0530

    Add Home Rout

--- server/app.js
+++ server/app.js
@@ -7,6 +7,13 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "api is runnign "
+    })
+})
+
 connectToMongodb()
 
 app.use('/api', useSupplier)
